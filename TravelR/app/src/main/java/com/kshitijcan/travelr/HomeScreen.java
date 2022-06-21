435dfd685c493f85cb7091fcc67ea8e6381cd21b
kshitij-halankar <halanka@uwindsor.ca>
2022-06-21 10:53:35 -0400

    Home Screen Tab Layout

---
+++ TravelR/app/src/main/java/com/kshitijcan/travelr/HomeScreen.java
@@ -0,0 +1,31 @@
+package com.kshitijcan.travelr;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class HomeScreen extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        tabLayout=findViewById(R.id.hometabs);
+        viewPager=findViewById(R.id.homeviewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        HomeViewPageAdapter homeViewPageAdapter=new HomeViewPageAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        homeViewPageAdapter.addFragment(new PlannerFragment(),"Planner");
+        homeViewPageAdapter.addFragment(new MyTripsFragment(),"My Trips");
+        homeViewPageAdapter.addFragment(new UtiltiesFragment(),"Utilities");
+        viewPager.setAdapter(homeViewPageAdapter);
+    }
+}
---
+++ TravelR/app/src/main/java/com/kshitijcan/travelr/HomeViewPageAdapter.java
@@ -0,0 +1,41 @@
+package com.kshitijcan.travelr;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeViewPageAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> fragmentList=new ArrayList<>();
+    private final List<String> fragmentTitle=new ArrayList<>();
+
+    public HomeViewPageAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        fragmentList.add(fragment);
+        fragmentTitle.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle.get(position);
+    }
+}
