cf35d49aa651517d00b7fed849d09d1fdf56b15d
kshitij-halankar <halanka@uwindsor.ca>
2022-06-24 15:10:30 -0400

    Travel Planner Part 1 UI

--- TravelR/app/src/main/java/com/kshitijcan/travelr/TravelPlanner.java
+++ TravelR/app/src/main/java/com/kshitijcan/travelr/TravelPlanner.java
@@ -3,17 +3,63 @@ package com.kshitijcan.travelr;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class TravelPlanner extends AppCompatActivity {
 
-
+    TextView fromDate, toDate;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_planner);
 
+
+        fromDate=(TextView) findViewById(R.id.fromdate);
+        toDate=(TextView) findViewById(R.id.todate);
+
+        Calendar calendar=Calendar.getInstance();
+        final int year=calendar.get(Calendar.YEAR);
+        final int month=calendar.get(Calendar.MONTH);
+        final int day=calendar.get(Calendar.DAY_OF_MONTH);
+
+        toDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog=new DatePickerDialog(TravelPlanner.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        i1=i1+1;
+                        String date=i2+"/"+i1+"/"+i;
+                        toDate.setText(date);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        fromDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog=new DatePickerDialog(TravelPlanner.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        i1=i1+1;
+                        String date=i2+"/"+i1+"/"+i;
+                        fromDate.setText(date);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
 
     }
 }
